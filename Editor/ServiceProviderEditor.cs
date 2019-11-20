035989e78d79a7ec0c50341996f391621147073e
Cory Leach <cory.leach@gmail.com>
2019-11-20 18:24:19 -0500

    Fix warnings.

---
+++ Editor/ServiceProviderEditor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameframe.ServiceProvider.Editor
+{
+    public class ServiceProviderEditor : UnityEditor.Editor
+    {
+    }
+}
+
+
